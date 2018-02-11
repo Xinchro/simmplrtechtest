9a85a83678874e5e3fb15803badd05695df65f6d
Xinchro <TheXrocks@gmail.com>
2018-02-11 23:06:41 +0000

    implemented moment date listing for the current month

--- src/monthgrid.js
+++ src/monthgrid.js
@@ -3,8 +3,9 @@ import moment from 'moment'
 
 class MonthGrid extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     
+    // default, temp, calendar structure
     let arr = []
     for(let i=-2; i <= 34; i++) {
       if(i===0) i++
@@ -15,13 +16,37 @@ class MonthGrid extends React.Component {
     }
 
     this.state = {
-      currentMonth: {
-        name: "January",
+      current: {
+        month: "January",
         date: 1,
         day: "Monday"
       },
       days: arr
     }
+
+    this.state.days = this.setCurrentDays()
+    this.state.current.month = moment().format("MMMM")
+    this.state.current.date = moment().format("D")
+    this.state.current.day = moment().format("dddd")
+
+    console.log(this.state.days)
+  }
+
+  setCurrentDays() {
+    let dates = []
+
+    const preDays = Math.floor((36-moment().daysInMonth())/2)
+    const postDays = Math.ceil((36-moment().daysInMonth())/2)
+
+    console.log(preDays, postDays)
+
+    for(let i=-preDays; i<36-postDays; i++) {
+      // +1 because moment is zero indexed, which "adds" 1 to preDays and "removes" to postDays
+      let date = moment().date(i+1).format("D")
+      dates.push(` [${date}] `)
+    }
+
+    return dates
   }
 
   componentWillMount() {
@@ -30,9 +55,11 @@ class MonthGrid extends React.Component {
   render() {
     return (
       <div className="">
-        <header>{ this.state.currentMonth.name }</header>
-        <ul>
-          {this.state.days}
+        <header>{ this.state.current.month }, { this.state.current.date }, { this.state.current.day }</header>
+        <ul className="monthGrid">
+          <li className="monthDay">
+            {this.state.days}
+          </li>
         </ul>
       </div>
     )
