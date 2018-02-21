80e0e51ed74cb4b59c1f69daa452cc25f9c8959c
Xinchro <thexrocks@gmail.com>
2018-02-21 16:24:53 +0000

    implemented very basic hash router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
+import { HashRouter } from 'react-router-dom';
+
 import './index.html'
+
 
 
 $(".dropdown-button").dropdown()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <HashRouter>
+    <App />
+  </HashRouter>
+), document.getElementById('root'));
