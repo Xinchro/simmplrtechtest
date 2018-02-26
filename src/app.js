9505c5f79a95c3a5275098e5fcc0183d9a6d0bbc
Xinchro <thexrocks@gmail.com>
2018-02-26 08:38:45 +0000

    added some safeguards for malformed URLs

--- src/app.js
+++ src/app.js
@@ -32,16 +32,11 @@ class Calendar extends React.Component {
       }
     }
 
-    // get year from params, if available
-    let year = props.match.params.year ? props.match.params.year : moment().format("YYYY")
-    // get month from params, if available
-    let month = props.match.params.month ? props.match.params.month : moment().format("MMMM")
-    this.state.date.days = this.getCurrentDays(year, month)
-
+    this.state.date.days = this.getCurrentDays(this.getUrlParams().year, this.getUrlParams().month)
 
     this.state.date.listDates = this.state.date.days.map((date, index) => {
       const diff = date.overflow ? "lighten-1" : "darken-1"
-      const color = this.isCurrentDate(year, month, date.date) ? "deep-purple" : "blue-grey"
+      const color = this.isCurrentDate(this.getUrlParams().year, this.getUrlParams().month, date.date) ? "deep-purple" : "blue-grey"
       const classes = `monthDay card ${color} ${diff} col s4 m2`
 
       return (
@@ -54,6 +49,47 @@ class Calendar extends React.Component {
     this.getDate = this.getDate.bind(this)
     this.changeMonth = this.changeMonth.bind(this)
     this.changeYear = this.changeYear.bind(this)
+  }
+
+  getCurrentYear() {
+    return moment().format("YYYY")
+  }
+
+  getCurrentMonth() {
+    return moment().format("MMMM")
+  }
+
+  getUrlParams() {
+    const months = "JanuaryFebruaryMarchAprilMayJuneJulyAugustSeptemberOctoberNovemberDecember"
+    const minYear = 1950
+    const maxYear = 2049
+
+    try {
+      if(!months.includes(this.props.match.params.month)) {
+        throw "Unsupported month!"
+      }
+
+      if(isNaN(parseInt(this.props.match.params.year))) {
+        throw "Unsupported year!"
+      } else {
+        if(parseInt(this.props.match.params.year) < 1950
+           || parseInt(this.props.match.params.year) > 2049) {
+          throw "Year out of supported range!"
+        }
+      }
+
+      // get year from params, if available
+      const urlYear = this.props.match.params.year ? this.props.match.params.year : this.getCurrentYear()
+
+      // get month from params, if available
+      const urlMonth = this.props.match.params.month ? this.props.match.params.month : this.getCurrentMonth()
+
+      return { year: urlYear, month: urlMonth }
+    } catch(err) {
+      console.error(err)
+    }
+
+    return { year: this.getCurrentYear(), month: this.getCurrentMonth() }
   }
 
   getCurrentDays(inYear, inMonth) {
@@ -77,23 +113,20 @@ class Calendar extends React.Component {
   }
 
   getDate() {
-    let currentYear = this.props.match.params.year ? this.props.match.params.year : moment().format("YYYY")
-    let currentMonth = this.props.match.params.month ? this.props.match.params.month : moment().format("MMMM")
-
     let date = {
       current: {
-        year: currentYear,
-        month: currentMonth,
+        year: this.getUrlParams().year,
+        month: this.getUrlParams().month,
         date: moment().format("D"),
         day: moment().format("dddd")
       }
     }
 
-    date.days = this.getCurrentDays(currentYear, currentMonth)
+    date.days = this.getCurrentDays(this.getUrlParams().year, this.getUrlParams().month)
 
     date.listDates = date.days.map((date, index) => {
       const diff = date.overflow ? "lighten-1" : "darken-1"
-      const color = this.isCurrentDate(currentYear, currentMonth, date.date) ? "deep-purple" : "blue-grey"
+      const color = this.isCurrentDate(this.getUrlParams().year, this.getUrlParams().month, date.date) ? "deep-purple" : "blue-grey"
       const classes = `monthDay card ${color} ${diff} col s4 m2`
 
       return (
